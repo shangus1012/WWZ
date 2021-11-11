42dcb773564ed00a5b42e7d8c86185c71dc348e8
shangus1012 <91159096+shangus1012@users.noreply.github.com>
2021-11-12 00:14:24 +0900

    new webpage

---
+++ js/nav.js
@@ -0,0 +1,116 @@
+/**
+ * nav 태그를 출력해 주는 자바스크립트파일
+ */
+var nav, div1, div2;
+
+//=====================================================================================
+function button() {//반응형 웹페이지 구현
+   var button = document.createElement("button");
+   button.setAttribute('type', 'button');
+   button.setAttribute('class', 'navbar-toggler');
+   button.setAttribute('type', 'button');
+   button.setAttribute('data-toggle', 'collapse');
+   button.setAttribute('data-target', '#navbarNav');
+   button.setAttribute('aria-controls', 'navbarNav');
+   button.setAttribute('aria-expanded', 'false');
+   button.setAttribute('aria-label', 'Toggle navigation');
+   button.innerHTML += "<span class='navbar-toggler-icon'></span>"
+   nav.appendChild(button);
+}
+
+function pageList(pageNum) {//탭목록 구현
+   var div = document.createElement('div');
+   div.setAttribute('class', 'collapse navbar-collapse');
+   div.setAttribute('id', 'navbarNav');
+   var ul = document.createElement('ul');
+   ul.setAttribute('class', 'nav nav-tabs mr-auto');
+
+   var A = document.createElement('li');
+   var B = document.createElement('li');
+   var C = document.createElement('li');
+   var D = document.createElement('li');
+
+   var E = document.createElement('li');
+   var F = document.createElement('li');
+   var G = document.createElement('li');
+   var H = document.createElement('li');
+
+   A.setAttribute('class', 'nav-item');
+   B.setAttribute('class', 'nav-item');
+   C.setAttribute('class', 'nav-item');
+   D.setAttribute('class', 'nav-item');
+   E.setAttribute('class', 'nav-item');
+   F.setAttribute('class', 'nav-item');
+   G.setAttribute('class', 'nav-item');
+   H.setAttribute('class', 'nav-item');
+
+   A.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(1)'>총잡이</a>";
+   B.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(2)'>난동꾼</a>";
+   C.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(3)'>위생병</a>";
+   D.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(4)'>해결사</a>";
+
+   E.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(5)'>학살자</a>";
+   F.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(6)'>파괴자</a>";
+   G.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(7)'>드론마스터</a>";
+   H.innerHTML += "<a class='nav-link' href='javascript:void(0);' onclick='createNav(8)'>방패병</a>";
+
+   if (pageNum == 1) {
+      A.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(1)'>총잡이</a>";
+   }
+   else if (pageNum == 2) {
+      B.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(2)'>난동꾼</a>";
+   }
+   else if (pageNum == 3) {
+      C.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(3)'>위생병</a>";
+   }
+   else if (pageNum == 4) {
+      D.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(4)'>해결사</a>";
+   }
+   else if (pageNum == 5) {
+      E.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(5)'>학살자</a>";
+   }
+   else if (pageNum == 6) {
+      F.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(6)'>파괴자</a>";
+   }
+   else if (pageNum == 7) {
+      G.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(7)'>드론마스터</a>";
+   }
+   else if (pageNum == 8) {
+      H.innerHTML = "<a class='nav-link active' href='javascript:void(0);' onclick='createNav(8)'>방패병</a>";
+   }
+   else {
+      alert("error! pageNum = " + pageNum);
+   }
+
+   ul.appendChild(A);
+   ul.appendChild(B);
+   ul.appendChild(C);
+   ul.appendChild(D);
+   ul.appendChild(E);
+   ul.appendChild(F);
+   ul.appendChild(G);
+   ul.appendChild(H);
+   div.appendChild(ul);
+
+
+   nav.appendChild(div);
+}
+//==================================================================================
+
+function createNav(pageNum) { // nav를 생성하는 함수
+   nav = document.getElementById("nav");
+   nav.innerHTML = "";
+   nav.setAttribute('class', 'navbar navbar-expand-lg navbar-light fixed-top');
+
+   nav.innerHTML += "<a class='navbar-brand' href='https://github.com/shangus1012/manga'>월드워Z</a>";
+   button();//function
+   pageList(pageNum);//function
+   createTable(pageNum);
+}
+
+/*
+    source : https://github.com/shangus1012/manga
+    제작자 : 만갤 여주학대만화추천(rmh0102)
+    모든 소스코드는 직접 작성한 것이고, 출처는 굳이 지워야만 한다면 상관 없습니다.
+    누군가 물어본다면 정보 공유를 위해서 출처를 표기해 주세요. 감사합니다.
+*/
---
+++ js/table.js
@@ -0,0 +1,103 @@
+var classnum;
+function createTable(pageNum) {
+    classnum = pageNum;
+    var className = numToName();
+    for (var n = 0; n < 3; n++) {
+        for (var i = 1; i < 30; i++) {
+            if (i % 10) {
+                var target = document.getElementsByClassName(i);
+                target[n].setAttribute("onclick", "select(" + n + "," + i + ");saveTable();");
+                target[n].setAttribute("class", i);
+                target[n].setAttribute("name", n);
+                target[n].innerHTML = "<img src='media/" + className + "/" + i + ".png' alt = '" + className + n + i + "'>";
+            }
+            if (i < 9) {
+                var target = document.getElementsByClassName("red" + i);
+                target[n].setAttribute("onclick", "select('red" + i + "');");
+                target[n].innerHTML = "<img src='media/" + className + "/red" + i + ".png' alt = '" + className + n + i + "'>";
+            }
+        }
+    }
+    loadTable();
+}
+
+function select(n, i) {
+    var temp = (i % 10 - 1) % 3;
+    var init = i - temp;
+    for (var num = 0; num < 3; num++) {
+        var target = document.getElementsByClassName(init + num);
+        target[n].setAttribute("class", init + num);
+    }
+    var target = document.getElementsByClassName(i);
+    target[n].setAttribute("class", i + " clicked");
+}
+
+function saveTable() {
+    var className = numToName();
+    var arr = incode();
+    for (var i = 0; i < 3; i++) {
+        setCookie(className + i, arr[i], 30);
+    }
+}
+
+function loadTable() {
+    var className = numToName();
+    for (var n = 0; n < 3; n++) {
+        var cookie = getCookie(className + n);
+        var arr = decode(cookie);
+        for (var i = 0; i < arr.length - 1; i++) {
+            select(n, arr[i]);
+        }
+    }
+}
+
+function incode() {
+    var target = document.getElementsByClassName("clicked");
+    var arr = ["", "", ""];
+    for (var i = 0; i < target.length; i++) {
+        var temp = target[i].className.split(" ");
+        if (target[i].getAttribute("name") == 0) {
+            arr[0] += temp[0] + "/";
+        }
+        else if (target[i].getAttribute("name") == 1) {
+            arr[1] += temp[0] + "/";
+        }
+        else if (target[i].getAttribute("name") == 2) {
+            arr[2] += temp[0] + "/";
+        }
+    }
+    return arr;
+}
+
+function decode(string) {
+    return string.split("/");
+}
+
+function numToName() {
+    var className = "";
+    if (classnum == 1) {
+        className = "gunslinger";
+    }
+    else if (classnum == 2) {
+        className = "Hellraiser";
+    }
+    else if (classnum == 3) {
+        className = "Medic";
+    }
+    else if (classnum == 4) {
+        className = "Fixer";
+    }
+    else if (classnum == 5) {
+        className = "Slasher";
+    }
+    else if (classnum == 6) {
+        className = "Exterminator";
+    }
+    else if (classnum == 7) {
+        className = "Dronemaster";
+    }
+    else if (classnum == 8) {
+        className = "Vanguard";
+    }
+    return className;
+}
